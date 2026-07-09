cce544768df5705dc670f4005e65f2386782e9de
editorsatish <editorkimssikhara@gmail.com>
2026-07-10 00:56:17 +0530

    Initial Portfolio Website

--- script.js
+++ script.js
@@ -294,3 +294,40 @@ year.innerHTML=`© ${currentYear} Tadi Kumar Satish Reddy. All Rights Reserved.`
 }
 
 console.log("Portfolio Loaded Successfully");
+// Portfolio Filter
+
+const filterButtons=document.querySelectorAll(".filter-btn");
+
+const projects=document.querySelectorAll(".project-card");
+
+filterButtons.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+document.querySelector(".filter-btn.active").classList.remove("active");
+
+button.classList.add("active");
+
+const filter=button.dataset.filter;
+
+projects.forEach(project=>{
+
+if(filter==="all"){
+
+project.style.display="block";
+
+}else if(project.classList.contains(filter)){
+
+project.style.display="block";
+
+}else{
+
+project.style.display="none";
+
+}
+
+});
+
+});
+
+});
